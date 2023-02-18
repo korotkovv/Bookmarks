0ee455a1b6891db438c1d0ef7f0986e929261874
Korotkov Vitaliy <korotkov.vitalii@gmail.com>
2023-02-18 14:08:31 +0300

    мелкие правки

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import glob from 'glob';
 
 // https://vitejs.dev/config/
-
 export default ({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 	return defineConfig({
 		base: '/',
-
-		build: {
-			emptyOutDir: true,
-			outDir: path.join(__dirname, process.env.VITE_DIST_DIR),
-			rollupOptions: {
-				input: glob.sync(path.resolve(__dirname, './src', '*.html')),
-			},
-		},
 		plugins: [vue()],
 		resolve: {
 			alias: {
